06e32b60cced34e0f6baa3c8756fa97a5c2163a4
egocentri <d.kravchenko@naash.ru>
2024-12-22 20:20:17 +0300

    Updated main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@
 
 import (
     "github.com/gin-gonic/gin"
-    "github.com/egocentri/go-webcalc/package_web/handler.go"
+    "github.com/egocentri/go-webcalc/package_web/handler"
 func main() {
 	a := gin.Default()
 	a.POST("/api/v1/calculate", handler.calculateHandler)
