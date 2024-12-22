3b800dfc13ed71500718845804aeef0157ccd998
egocentri <d.kravchenko@naash.ru>
2024-12-22 20:32:56 +0300

    Create main.go

---
+++ cmd/calc_service/main.go
@@ -0,0 +1,10 @@
+package last
+
+import (
+    "github.com/gin-gonic/gin"
+    "github.com/egocentri/go-webcalc/package_web/handler"
+func main() {
+	a := gin.Default()
+	a.POST("/api/v1/calculate", handler.calculateHandler)
+	a.Run(":8080")
+}
